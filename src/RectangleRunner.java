79a9e255d766ec5e47540fefd65513bada83a12e
stephanied117 <stephanied117@nycstudents.net>
2023-09-28 18:43:04 -0400

    added print statements

--- src/RectangleRunner.java
+++ src/RectangleRunner.java
@@ -5,8 +5,8 @@ public class RectangleRunner {
         System.out.println("Plot 1 Length: " + getLength);
         int getWidth = plot1.getWidth();
         System.out.println("Plot 1 Width: " + getWidth);
-        int calcArea = plot1.calculateArea();
-        System.out.println("Plot 1 Area: " + calcArea);
+        int calcArea1 = plot1.calculateArea();
+        System.out.println("Plot 1 Area: " + calcArea1);
 
 
         Rectangle plot2 = new Rectangle(125);
@@ -14,8 +14,8 @@ public class RectangleRunner {
         System.out.println("Plot 2 Length: " + getLength);
         getWidth = plot2.getWidth();
         System.out.println("Plot 2 Width: " + getWidth);
-        calcArea = plot2.calculateArea();
-        System.out.println("Plot 2 Area: " + calcArea);
+        int calcArea2 = plot2.calculateArea();
+        System.out.println("Plot 2 Area: " + calcArea2);
 
 
         Rectangle plot3 = new Rectangle();
@@ -23,8 +23,8 @@ public class RectangleRunner {
         System.out.println("Plot 3 Length: " + getLength);
         getWidth = plot3.getWidth();
         System.out.println("Plot 3 Width: " + getWidth);
-        calcArea = plot3.calculateArea();
-        System.out.println("Plot 3 Area: " + calcArea);
+        int calcArea3 = plot3.calculateArea();
+        System.out.println("Plot 3 Area: " + calcArea3);
 
 
         plot1.setWidth(75);
@@ -37,24 +37,26 @@ public class RectangleRunner {
         System.out.println("Plot 1 Length: " + getLength);
         getWidth = plot1.getWidth();
         System.out.println("Plot 1 Width: " + getWidth);
-        calcArea = plot1.calculateArea();
-        System.out.println("Plot 1 Area: " + calcArea);
+        calcArea1 = plot1.calculateArea();
+        System.out.println("Plot 1 Area: " + calcArea1);
 
 
         getLength = plot2.getLength();
         System.out.println("Plot 2 Length: " + getLength);
         getWidth = plot2.getWidth();
         System.out.println("Plot 2 Width: " + getWidth);
-        calcArea = plot2.calculateArea();
-        System.out.println("Plot 2 Area: " + calcArea);
+        calcArea2 = plot2.calculateArea();
+        System.out.println("Plot 2 Area: " + calcArea2);
 
 
         getLength = plot3.getLength();
         System.out.println("Plot 3 Length: " + getLength);
         getWidth = plot3.getWidth();
         System.out.println("Plot 3 Width: " + getWidth);
-        calcArea = plot3.calculateArea();
-        System.out.println("Plot 3 Area: " + calcArea);
+        calcArea3 = plot3.calculateArea();
+        System.out.println("Plot 3 Area: " + calcArea3);
+
+        System.out.println("These three plots require " + (calcArea1 + calcArea2 + calcArea3) + " square feet of seed.");
     }
 }
 
